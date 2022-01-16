63e9179d19314f85d33c276737338fa7210e3307
GouravIN <gourav.pattnaik@internations.org>
2022-01-16 21:46:51 +0100

    Deploy multiple servers

--- instance.tf
+++ instance.tf
@@ -1,4 +1,19 @@
-resource "aws_instance" "my_instance" {
-  ami           = lookup(var.AWS_AMI, var.AWS_REGION )
+resource "aws_instance" "frontend" {
+  count = 1
+  depends_on = [aws_instance.backend]
+  ami           = var.AWS_AMI
   instance_type = var.AWS_INSTANCE_TYPE
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_instance" "backend" {
+  count = 2
+  ami = var.AWS_AMI
+  instance_type = var.AWS_INSTANCE_TYPE
+  timeouts {
+    create = "60m"
+    delete = "2h"
+  }
 }
--- vars.tf
+++ vars.tf
@@ -4,12 +4,7 @@ variable "AWS_REGION" {
   default = "eu-central-1"
 }
 variable "AWS_AMI" {
-  type = map(string)
-  default = {
-    eu-central-1 = "ami-05cafdf7c9f772ad2"
-    eu-west-1 = "ami-0874dad5025ca362c"
-    eu-west-3 = "ami-04e905a52ec8010b2"
-  }
+  default = "ami-05cafdf7c9f772ad2"
 }
 variable "AWS_INSTANCE_TYPE" {
   default = "t2.micro"
